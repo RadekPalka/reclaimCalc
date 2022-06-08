d5fa7080fb2c26236b2e69f5f2b74518e464dcd1
Radoslaw Palka <radekpalka@gmail..com>
2022-06-08 21:00:08 +0200

    add addEverethibg buttton and function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const reclaimList = [...document.querySelectorAll('li')]
+const addBtn = document.querySelector('button.sum')
 console.log(reclaimList)
 const reclaimObjectList= []
 reclaimList.forEach(el =>{
   reclaimObjectList.push(new ReclaimElement(el))
 })
 console.log(reclaimObjectList)
+addBtn.addEventListener('click', ()=>{
+  console.log(reclaimObjectList)
+  const sumOfEverything = reclaimObjectList.reduce((prev, next)=> prev + next.sum,0)
+  console.log(sumOfEverything)
+})
